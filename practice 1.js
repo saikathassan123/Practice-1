c1c49340a592bdf3e55d9d33d0cb9ef3d84892c6
Md. Saikat Hassan <saikat2day@gmail.com>
2025-12-06 12:56:32 +0600

    Add files via upload

---
+++ practice 1.js
@@ -0,0 +1,29 @@
+function monthlySavings(payments, livingCost) {
+  
+  if (!Array.isArray(payments) || typeof livingCost !== "number") {
+    return "invalid input";
+  }
+
+  let totalAfterTax = 0;
+
+  for (let payment of payments) {
+    if (payment >= 3000) {
+      totalAfterTax += payment * 0.8;
+    } else {
+      totalAfterTax += payment;
+    }
+  }
+
+  let savings = totalAfterTax - livingCost;
+
+  if (savings < 0) {
+    return "earn more";
+  }
+  return savings;
+}
+
+// Sample Test Cases
+console.log(monthlySavings([1000, 2000, 3000], 5400)); 
+console.log(monthlySavings([1000, 2000, 2500], 5000)); 
+console.log(monthlySavings([900, 2700, 3400], 10000)); 
+console.log(monthlySavings(100, [900, 2700, 3400])); 
