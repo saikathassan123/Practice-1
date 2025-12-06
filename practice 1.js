5c1a8913dfa39f98ec8038dfade400ff3966a00d
Md. Saikat Hassan <saikat2day@gmail.com>
2025-12-06 12:57:36 +0600

    practice 1.js

--- practice 1.js
+++ practice 1.js
@@ -22,8 +22,8 @@ function monthlySavings(payments, livingCost) {
   return savings;
 }
 
-// Sample Test Cases
 console.log(monthlySavings([1000, 2000, 3000], 5400)); 
 console.log(monthlySavings([1000, 2000, 2500], 5000)); 
 console.log(monthlySavings([900, 2700, 3400], 10000)); 
 console.log(monthlySavings(100, [900, 2700, 3400])); 
+
